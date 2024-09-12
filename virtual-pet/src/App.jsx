ae598994e3dbc9f1f0a61b246f3fff269a53db09
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-09-12 11:53:36 -0400

    implement title and stat divs (html only)

--- virtual-pet/src/App.jsx
+++ virtual-pet/src/App.jsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="main-container">
+        <nav className="navbar">Virtual Pet</nav>
+        <div className="main-content">
+          <div className="stats">
+            <div className="hunger-stat">Hunger: _/10</div>
+            <div className="happiness-stat">Happiness: _/10</div>
+            <div className="energy-stat">Energy: _/10</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
